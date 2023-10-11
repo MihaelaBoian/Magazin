e3d409667a46c9f4332232cb79bd9bb8ddd0a7a2
MihaelaBoian <pinzari_m@mail.ru>
2023-10-11 14:54:31 +0300

    l

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Invoice {
+public class Invoice {
+    private String model;
+    private String descriere;
+    private int cantitate;
+    private double pret;
+   // public double amount;
+
+//    public Invoice(String m,String d, int c, double p)
+//    {   model=m;
+//        descriere=d;
+//        cantitate=c;
+//        pret=p;
+//    }
+    public String getModel(){
+        return model;
+    }
+    public void setModel(String model){
+        this.model=model;
+    }
+    public String getDescriere(){
+        return descriere;
+    }
+    public void setDescriere(String descriere){
+        this.descriere=descriere;
+    }
+    public int getCantitate(){
+        return cantitate;
+    }
+    public void setCantitate(int cantitate) {
+        if (cantitate < 0) {
+            this.cantitate = 0;
+            System.out.println("Cantitate neidentificata");
+        } else {
+            this.cantitate = cantitate;
+        }
+    }
+        public double getPret(){
+            return pret;
+
+
+    }
+    public void setPret(double pret) {
+        if (pret < 0) {
+            this.pret = 0.0;
+            System.out.println("Pret inexistent");
+        } else {
+             this.pret = pret;
+        }
+    }
+    public double getAmount(){
+        double s_factura;
+                     s_factura=cantitate*pret;
+        return s_factura;
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+Invoice a=new Invoice();
+a.setPret(2);
+a.setCantitate(-2);
+a.setModel("Pixel");
+a.setDescriere("Google Pixel");
+        System.out.println("Model: "+a.getModel()+"  Descriere: "+a.getDescriere()+"  Cantitate: "+a.getCantitate()+"  Suma totala achitata : "+a.getAmount());
+ a.setCantitate(5);
+ a.setPret(65.02);
+        System.out.println("Model: "+a.getModel()+"  Descriere: "+a.getDescriere()+"  Cantitate: "+a.getCantitate()+"  Suma totala achitata : "+a.getAmount());
     }
 }
